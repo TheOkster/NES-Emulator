c3c5c5624f632c834bdfd664b1c702fe9257b16f
Joshua Oki <jokioluda@gmail.com>
2025-10-31 16:45:35 -0400

    Slightly changed some ppu comments

--- hdl/ppu.sv
+++ hdl/ppu.sv
@@ -32,38 +32,40 @@ module ppu (
             oam_dma <= 0;
         end else begin
             // I think? this is right - ignoring timing ofc
-            // TODO: I think this may only be permitted during blank periods, need to double check
+            // TODO: This may only be permitted during blank periods, need to double check
             // and if necessary, only do it during these times
-            if(cpu_rw == CPU_WRITE) begin
-                case (cpu_addr) // May need to cahnge to be 3 bits and change the 16' to 3' accordingly
-                    16'h2000: ppu_ctrl <= cpu_dout;
-                    16'h2001: ppu_mask <= cpu_dout;
-                    16'h2003: oam_addr <= cpu_dout;
-                    16'h2004: oam_data <= cpu_dout;
-                    16'h2005: begin 
-                        if(w) begin
-                            ppu_scroll[15:8] <= cpu_dout;
-                        end else begin
-                            ppu_scroll[7:0] <= cpu_dout;
-                        end
-                    end // handle later
-                    16'h2006: begin 
-                        if(w) begin
-                            ppu_addr[15:8] <= cpu_dout;
-                        end else begin
-                            ppu_addr[7:0] <= cpu_dout;
-                        end
-                    end // handle later
-                    16'h2007: ppu_data <= cpu_dout;
-                    16'h4014: oam_dma <= cpu_dout;
-                endcase
-            end
-            else begin
-                case (cpu_addr)
-                    16'h2002: cpu_din <= ppu_status; // Only [7:5] actually matter, could optimize?
-                    16'h2004: cpu_din <= oam_data;
-                    16'h2007: cpu_din <= ppu_data;
-                endcase
+            if(8'h2000 <= addr && addr <= 8'h3FFF) begin
+                if(cpu_rw == CPU_WRITE) begin
+                    // 16'h4014: oam_dma <= cpu_dout; // but what about this? -> needs to be in separate if statements
+                    case (cpu_addr[2:0]) // Changed to be 3 bits but keeping 16 bit input for mirroring
+                        3'h0: ppu_ctrl <= cpu_dout;
+                        3'h1: ppu_mask <= cpu_dout;
+                        3'h3: oam_addr <= cpu_dout;
+                        3'h4: oam_data <= cpu_dout;
+                        3'h5: begin 
+                            if(w) begin
+                                ppu_scroll[15:8] <= cpu_dout;
+                            end else begin
+                                ppu_scroll[7:0] <= cpu_dout;
+                            end
+                        end // handle later
+                        3'h6: begin 
+                            if(w) begin
+                                ppu_addr[15:8] <= cpu_dout;
+                            end else begin
+                                ppu_addr[7:0] <= cpu_dout;
+                            end
+                        end // handle later
+                        3'h7: ppu_data <= cpu_dout;
+                    endcase
+                end
+                else begin
+                    case (cpu_addr)
+                        16'h2002: cpu_din <= ppu_status; // Only [7:5] actually matter, could optimize?
+                        16'h2004: cpu_din <= oam_data;
+                        16'h2007: cpu_din <= ppu_data;
+                    endcase
+                end
             end
         end
     end
