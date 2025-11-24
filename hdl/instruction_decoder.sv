836fa409a852f769557701102cc04b1fdde7bbb4
sasha <sashafefelova04@gmail.com>
2025-11-24 01:43:01 -0500

    decoding for more regular instructions, still need to do group 00 including jumps and branches

--- hdl/instruction_decoder.sv
+++ hdl/instruction_decoder.sv
@@ -3,15 +3,17 @@
 // define shared enums for addressing mode and opcode
 package cpu_instructions; 
     typedef enum {
-        DX, 
-        DY, 
-        AX,
-        AY, 
+        ZERO_PAGE_X, 
+        ZERO_PAGE_Y, 
+        ABSOLUTE_X,
+        ABSOLUTE_Y, 
         INDEXED_INDIRECT, 
         INDIRECT_INDEXED, 
-        A, 
+        ACCUMULATOR, 
         IMMEDIATE, 
-        D, 
+        ZERO_PAGE, 
+        ABSOLUTE,
+        TODO,
         UNSUPPORTED
     } addressing_mode; 
     // D = Zero page
@@ -79,21 +81,108 @@ package cpu_instructions;
 
 endpackage
 
-
+// decoding reference: https://llx.com/Neil/a2/opcodes.html
 module instruction_decoder (
         input wire clk,
         input wire rst,
         
-        input wire [8:0] instruction,
+        input wire [7:0] instruction,
 
-        output logic [5:0] addressing_mode, 
-        output logic [5:0] opcode;
+        output logic [6:0] addressing_mode, 
+        output logic [6:0] opcode;
 
         output logic [7:0] dout,
     );
     logic [2:0] aaa;
     logic [2:0] bbb;
     logic [1:0] cc;
+
+    assign aaa = instruction[7:5];
+    assign bbb = instruction[4:2];
+    assign cc = instruction[1:0];
+
+    always_comb begin
+        case(cc)
+            2'b00: begin
+
+            end
+            2'b01: begin
+                // decode opcode
+                case(aaa) 
+                    3'b000: opcode <= ORA; 
+                    3'b001: opcode <= AND;
+                    3'b010: opcode <= EOR;
+                    3'b011: opcode <= ADC; 
+                    3'b100: opcode <= STA;
+                    3'b101: opcode <= LDA;
+                    3'b110: opcode <= CMP;
+                    3'b111: opcode <= SBC;
+                    default: opcode <= UNSUPPORTED;
+                endcase
+
+                // decode addressing mode
+                case(bbb) 
+                    3'b000: addressing_mode <= INDEXED_INDIRECT; 
+                    3'b001: addressing_mode <= ZERO_PAGE;
+                    3'b010: begin
+                        if (aaa == 3'b100) addressing_mode <= UNSUPPORTED;
+                        else addressing_mode <= IMMEDIATE;
+                    end 
+                    3'b011: addressing_mode <= ABSOLUTE; 
+                    3'b100: addressing_mode <= INDIRECT_INDEXED;
+                    3'b101: addressing_mode <= ZERO_PAGE_X;
+                    3'b110: addressing_mode <= ABSOLUTE_Y;
+                    3'b111: addressing_mode <= ABSOLUTE_X;
+                    default: addressing_mode <= UNSUPPORTED;
+                endcase
+            end
+            2'b10: begin
+                // decode opcode
+                case(aaa) 
+                    3'b000: opcode <= ASL; 
+                    3'b001: opcode <= ROL;
+                    3'b010: opcode <= LSR;
+                    3'b011: opcode <= ROR; 
+                    3'b100: opcode <= STX;
+                    3'b101: opcode <= LDX;
+                    3'b110: opcode <= DEC;
+                    3'b111: opcode <= INC;
+                    default: opcode <= UNSUPPORTED;
+                endcase
+
+                // decode addressing mode
+                case(bbb) 
+                    3'b000: begin
+                        if (aaa == 3'b101) addressing_mode <= IMMEDIATE; 
+                        else addressing_mode <= UNSUPPORTED;
+                    end
+                    3'b001: addressing_mode <= ZERO_PAGE;
+                    3'b010: begin
+                        if (aaa[2] == 0'b0) addressing_mode <= ACCUMULATOR;
+                        else addressing_mode <= UNSUPPORTED;
+                    end
+                    3'b011: addressing_mode <= ABSOLUTE; 
+                    3'b101: begin
+                        if ((aaa == 3'b100) || (aaa == 3'b101)) addressing_mode <= ZERO_PAGE_Y;
+                        else addressing_mode <= ZERO_PAGE_X;
+                    end
+                    3'b111: 
+                        if (aaa == 3'b100) addressing_mode <= UNSUPPORTED;
+                        else if (aaa == 3'b101) addressing_mode <= ABSOLUTE_Y;
+                        else addressing_mode <= ABSOLUTE_X;
+                    default: addressing_mode <= UNSUPPORTED;
+                endcase
+            end
+            2'b11: begin
+                opcode <= UNSUPPORTED; 
+                addressing_mode <= UNSUPPORTED;
+            end
+        endcase
+
+    end
+
+
+    
 
 
 endmodule
